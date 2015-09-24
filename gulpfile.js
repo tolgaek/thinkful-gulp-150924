e6af144181d9cfe1cd6239ee5c8a3bddcffc1f5f
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-09-24 18:33:24 -0400

    Add changed plugin to minimize images gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-
+'use strict';
 
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
 
 gulp.task('minimizeImages', function() {
   return gulp.src('img/*.gif')
+    .pipe(changed('build'))
     .pipe(imagemin())
     .pipe(gulp.dest('build'));
 });
