bab573a0be299cf2cdd1e04a3a4a9acbf0994b3c
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-09-24 18:37:53 -0400

    Add cleaning task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,17 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var changed = require('gulp-changed');
+var del = require('del');
 
 gulp.task('minimizeImages', function() {
   return gulp.src('img/*.gif')
     .pipe(changed('build'))
     .pipe(imagemin())
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function(callback) {
+  del('build/*', callback); 
 });
 
 gulp.task('default', ['minimizeImages']);
